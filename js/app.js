8e56e50af5f1560d9ad1ce2095971dbc4dfeab8a
Ka Hei Lam <81654487+kaheilam@users.noreply.github.com>
2023-02-11 23:12:47 -0500

    Update New JavaScript file

--- js/app.js
+++ js/app.js
@@ -1,17 +1,15 @@
-const icon = document.querySelector('.hook-header .hook-nav-bar .nav-list .nav-icon');
-const mobile_menu = document.querySelector('.hook-header .hook-nav-bar .nav-list ul');
-const header = document.querySelector('.hook-header.container');
+const icon = document.querySelector('.banner .navbar .navList .navIcon');
+const mobile_menu = document.querySelector('.banner .navbar .navList ul');
+const menu_item = document.querySelectorAll('.banner .navbar .navList ul li a');
 
 icon.addEventListener('click', () => {
     icon.classList.toggle('active');
     mobile_menu.classList.toggle('active');
 })
 
-document.addEventListener('scroll', () => {
-    let scroll_pos = window.scrollY;
-    if (scroll_pos > 250) {
-        header.style.backgroundColor = "#343f4f";
-    }else if (scroll_pos < 0 ) {
-        header.style.backgroundColor = "rgba(24, 30, 35, 0.69)";
-    }
-})
+menu_item.forEach(item => {
+    item.addEventListener('click', () => {
+        icon.classList.toggle('active');
+        mobile_menu.classList.toggle('active');
+    });
+});
